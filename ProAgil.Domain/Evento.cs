430636e3b590d9e8d46aa842cd18f59b6b64529f
williamsoaresdelima <43095193+williamsoaresdelima@users.noreply.github.com>
2020-12-05 14:24:10 -0300

    models-dominio-Evento-Palestrante-RedeSocial-Lote

--- ProAgil.Domain/Evento.cs
+++ ProAgil.Domain/Evento.cs
@@ -16,6 +16,6 @@ namespace ProAgil.Domain
         public string Lote { get; set; }
         public List<Lote> Lotes { get; set;} 
         public List<RedeSocial> RedesSociais { get; set; }
-        public List<PalestranteEvento> PalestranteEventos { get; set; }
+        public List<PalestranteEvento> PalestrantesEventos { get; set; }
     }
 }
