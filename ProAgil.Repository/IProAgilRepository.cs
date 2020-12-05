0f7f02f51989c80e0b9cb1dbe674cabf2e4afb87
williamsoaresdelima <43095193+williamsoaresdelima@users.noreply.github.com>
2020-12-05 16:41:18 -0300

    interfce-contexto

--- ProAgil.Repository/IProAgilRepository.cs
+++ ProAgil.Repository/IProAgilRepository.cs
@@ -1,19 +1,25 @@
 using System.Threading.Tasks;
 using ProAgil.Domain;
 
-namespace .ProAgil.Repository
+namespace ProAgil.Repository
 {
     public interface IProAgilRepository
     {
-         void Add<T> (T entity) where T : class;
-         void Update<T> (T entity) where T : class;
-         void Delete<T> (T entity) where T : class;
-         Task<bool> SaveChangesAsync();
-         Task<Evento[]> GetAllEventoAsyncByTema(string tema, bool includePalestrantes);
-         Task<Evento[]> GetAllEventoAsync(bool includePalestrantes);
-         Task<Evento[]> GetAllEventoAsyncById(int EventoId, bool includePalestrantes);
+        //GERAL
+        void Add<T>(T entity) where T : class;
+        void Update<T>(T entity) where T : class;
+        void Delete<T>(T entity) where T : class;
 
-         Task<Evento[]> GetPalestrantesAsyncByName(bool includePalestrantes);
-         Task<Evento[]> GetAllEventoAsync(int PalestranteId, bool includePalestrantes);
+        Task<bool> SaveChangesAsync();
+
+        //EVENTOS
+        Task<Evento[]> GetAllEventoAsyncByTema(string tema, bool includePalestrantes);
+        Task<Evento[]> GetAllEventoAsync(bool includePalestrantes);
+        Task<Evento> GetAllEventoAsyncById(int EventoId, bool includePalestrantes);
+
+        Task<Palestrante> GetPalestranteAsync(int PalestranteId, bool includeEventos);
+        //PALESTRANTE
+        Task<Palestrante[]> GetPalestrantesAsyncByName(string name, bool includeEventos);
+
     }
 }
